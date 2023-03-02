a15e49e3b325c4f27117b77519b12671e17d187b
EmmanuelEyo <eyoemmanuel180@gmail.com>
2023-03-02 21:31:02 +0100

    Update repo

--- src/components/Maps.tsx
+++ src/components/Maps.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Maps = () => {
-  return <div className="bg-slate-800">Maps</div>;
+  return (
+    <div className="bg-slate-800">
+      <h1 className="uppercase">classic multiplayer match</h1>
+    </div>
+  );
 };
 
 export default Maps;
